ef3f44d80e516cb0a4f60a0ef97a30dd98d85dd3
Athanasia <athanasiakotopoulos@gmail.com>
2022-05-31 02:36:50 -0400

    updating employee test

---
+++ _tests_/employee.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../lib/Employee');
+const employee = new Employee('Martin','1','martinbradley@gmail.com');
+
+test('enter values', () => {
+    expect(employee.name).toBe('Martin');
+    expect(employee.id).toBe('1');
+    expect(employee.email).toBe('martinbradley@gmail.com')
+})
+
+test('name function', () => {
+    expect(employee.getName()).toBe('Martin');
+})
+
+test('ID function', () => {
+    expect(employee.getId()).toBe('1');
+})
+
+test('email function', () => {
+    expect(employee.getEmail()).toBe('martinbradley@gmail.com');
+})
